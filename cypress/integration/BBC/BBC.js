10d8c726d9d8fd85894a9b33a41162b83aa9ed86
john morris <fennymorris@live.co.uk>
2020-07-03 23:33:52 +0100

    add docker files

--- cypress/integration/BBC/BBC.js
+++ cypress/integration/BBC/BBC.js
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />
 
-import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
+import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
 
 Given('I open BBC News', () => {
   cy.visit('https://www.bbc.co.uk/news')
@@ -8,6 +8,4 @@ Given('I open BBC News', () => {
 
 Then('I see {string}" in the title', (title) => {
   cy.title().should('eq',title)
-  // expect(name).to.not.equal('Jane')
-
 })
--- cypress/integration/BBC/BBC.ts
+++
@@ -1,9 +0,0 @@
-import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
-
-Given('I open BBC News', () => {
-  // TODO: implement step
-});
-
-Then('I see {string}" in the title', (a: string) => {
-  // TODO: implement step
-});
