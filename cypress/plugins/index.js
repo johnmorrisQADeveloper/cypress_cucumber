981eedff824548647ab3d5e80d7b8bdc5a49f4aa
john morris <fennymorris@live.co.uk>
2020-07-13 23:22:49 +0100

    add fix for Viewport screenshot

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -3,5 +3,19 @@ const { addMatchImageSnapshotPlugin} = require('cypress-image-snapshot/plugin');
 
 module.exports = (on, config) => {
   on('file:preprocessor', cucumber()),
-  addMatchImageSnapshotPlugin(on, config);
+  addMatchImageSnapshotPlugin(on, config),
+  on('before:browser:launch', (browser = {}, args) => {
+    if (browser.name === 'chrome') {
+        args.push('--window-size=1280,1024');
+        // whatever you return here becomes the new args
+        return args
+    }
+    if (browser.name === 'electron') {
+        args['width'] = 1280
+        args['height'] = 1024
+        args['resizable'] = false;
+        // whatever you return here becomes the new args
+        return args
+    }
+  })
 }
