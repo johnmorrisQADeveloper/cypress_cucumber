0194942666141e4e3d53370db9de1decdec33bb5
john morris <fennymorris@live.co.uk>
2020-07-14 22:25:30 +0100

    update view port

--- cypress/integration/Conduit/Conduit.js
+++ cypress/integration/Conduit/Conduit.js
@@ -2,20 +2,25 @@
 import { Given, Then } from 'cypress-cucumber-preprocessor/steps'
 
 Given('I open Conduit', () => {
+  cy.viewport(1000, 644)
   cy.visit('/')
   if (Cypress.browser.name === 'chrome') {
+    cy.viewport(1000, 644)
     cy.matchImageSnapshot('homePage')
   }
 })
 
 Then('I see {string} in the title', (title) => {
+  cy.viewport(1000, 644)
   cy.title().should('be', 'Conduit')
   cy.visit('/#/login?_k=06oljf')
   if (Cypress.browser.name === 'chrome') {
+    cy.viewport(1000, 644)
     cy.matchImageSnapshot('pre-loginPage')
   }
   cy.login()
   if (Cypress.browser.name === 'chrome') {
+    cy.viewport(1000, 644)
     cy.matchImageSnapshot('post-loggedInPage')
   }
 })
