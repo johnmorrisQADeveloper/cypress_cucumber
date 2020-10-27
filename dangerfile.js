89c90bddf5836ca89494a47b6b182c0748e3cf09
john morris <fennymorris@live.co.uk>
2020-10-27 17:58:14 +0000

    sdfghjkl;'\

--- dangerfile.js
+++ dangerfile.js
@@ -1,3 +1,11 @@
-const { message, danger } = require('danger')
-const newFiles = danger.git.created_files.join('- ')
-message('New Files in this PR: \n - ' + newFiles)
+import { danger, warn } from 'danger'
+
+// No PR is too small to include a description of why you made a change
+if (danger.github.pr.body.length < 10) {
+  warn('Please include a description of your PR changes.')
+}
+
+// Check that someone has been assigned to this PR
+if (danger.github.pr.assignee === null) {
+  warn('Please assign someone to merge this PR, and optionally include people who should review.')
+}
