7eb7c4b2c6673eb069b457d30cd5a5639ea8ba29
john morris <fennymorris@live.co.uk>
2020-10-27 17:32:20 +0000

    asdfghjkl;

--- dangerfile.js
+++ dangerfile.js
@@ -1,3 +1,3 @@
-const { message, danger } = require('danger')
-const newFiles = danger.git.created_files
+import { message, danger } from 'danger'
+const newFiles = danger.git.created_files.join('- ')
 message('New Files in this PR: \n - ' + newFiles)
