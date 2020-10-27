8ec929d2c298e627352dcd06eed2780d3d9129ea
john morris <fennymorris@live.co.uk>
2020-10-27 18:56:28 +0000

    added more dangerjs info

--- dangerfile.js
+++ dangerfile.js
@@ -1,11 +1,17 @@
-const { danger, warn } = require('danger')
+import { fail, warn, message, markdown, danger } from 'danger'
+
+fail('This is a failure message')
+warn('This is a warning')
+message('This is a normal message')
+markdown('*Markdown* is also **supported**')
 
 // No PR is too small to include a description of why you made a change
 if (danger.github.pr.body.length < 10) {
   warn('Please include a description of your PR changes.')
 }
 
-console.log(danger.git.created_files)
+const { additions = 0, deletions = 0 } = danger.github.pr
+message(`:tada: The PR added ${additions} and removed ${deletions} lines.`)
 
 // Check that someone has been assigned to this PR
 if (danger.github.pr.assignee === null) {
